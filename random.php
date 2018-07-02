a37e88cde2aa8ae3fb7d847bd5f7090d212f5af0
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-07-02 14:23:56 +0700

    Update random.php

--- random.php
+++ random.php
@@ -111,7 +111,7 @@ foreach ($a as $a) {
 $zone = array("North :: ","BangKok :: ","Myanmar","Northeast and East ::","South and West :: ");
 
 $random = array_combine($zone,$a);
-	echo=$random;
+	echo $random;
 echo "<pre>";
    print_r($a);
 
