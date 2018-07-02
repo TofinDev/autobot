b274235b17db8f56a337a27c1e2e20561aad4e19
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-07-02 15:02:27 +0700

    Update random.php

--- random.php
+++ random.php
@@ -104,20 +104,20 @@ echo "The time is " . date("h:i:sa"). "<br><br>";
 	
 
 $zone = array("North :: ","BangKok :: ","Northeast and East ::","South and West :: ");
-shuffle($zone);
-foreach ($zone as $zone) {
-    echo "$zone ";
-}
-	
-$a=$_POST['chk'];
-shuffle($a);	
-foreach ($a as $a) {
-	
+echo "$zone ";
 echo "<pre>";
 	
    print_r($zone);
 
 echo "</pre>";	
+
+	
+	
+$a=$_POST['chk'];
+shuffle($a);	
+foreach ($a as $a) {
+	
+
 echo "<pre>";
 	
    print_r($a);
