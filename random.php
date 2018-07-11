2f417c7d71e2f6a1205728d4143653d1245211f8
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-07-11 10:11:35 +0700

    Update random.php

--- random.php
+++ random.php
@@ -107,9 +107,9 @@ echo "The time is " . date("h:i:sa"). "<br><br>";
 $zone = array("North :: ","BangKok :: ","Northeast and East ::","South and West :: ");
 	shuffle($zone);	
 foreach ($zone as $zone){
-<br>
+
 print_r($zone); 
-<br>		
+	
 }	
 $a=$_POST['chk'];
 shuffle($a);	
