459741ce3a67a353c07384b841ff078894c4b392
TofinDev <39640205+TofinDev@users.noreply.github.com>
2019-03-08 10:07:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -288,15 +288,15 @@ $(function(){
     $(".css_data_item").click(function(){  
         if($(this).prop("checked")==true){ 
             var indexObj=$(this).index(".css_data_item"); //     
-            if($(".css_data_item:checked").length>4){
+            if($(".css_data_item:checked").length>3){
             $(".css_data_item").not(":eq("+indexObj+")").prop( "checked", false ); 
             }
         }    
     });    
    
     $("#form_checkbox1").submit(function(){
-        if($(".css_data_item:checked").length<4){
-            alert("เลือกอย่างน้อย 4 คน");  
+        if($(".css_data_item:checked").length<3){
+            alert("เลือกอย่างน้อย 3 คน");  
 
             return false;       
         }    
